79b5328d84a694881b04640acf08a549f1e0d03a
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-19 21:47:27 +0100

    fix: backend block wrongly indented

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.0.0"
     }
   }
-}
 
-backend "s3" {
-  bucket         = "annomate-terraform-state"
-  key            = "lambda"
-  region         = "eu-central-1"
-  dynamodb_table = "terraform-lock"
+  backend "s3" {
+    bucket         = "annomate-terraform-state"
+    key            = "service/dev/test"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+  }
 }
