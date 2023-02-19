a6c20db597c5e2fc47a34428edf0719e5e524009
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-19 21:40:50 +0100

    fix: add provider

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -2,14 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0.0"
+      version = "~> 4.0"
     }
   }
+}
 
-  backend "s3" {
-    bucket         = "annomate-terraform-state"
-    key            = "lambda"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-lock"
-  }
+provider "aws" {
+  region = "eu-central-1"
+}
+
+backend "s3" {
+  bucket         = "annomate-terraform-state"
+  key            = "lambda"
+  region         = "eu-central-1"
+  dynamodb_table = "terraform-lock"
 }
