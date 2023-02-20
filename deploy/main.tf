89fc24786b9bf703086e11ca618585240cfb6682
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-20 12:27:40 +0100

    fix lambda handler for mangum

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda" {
   function_name = var.project_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "main.lambda_handler"
+  handler       = "app.main.handler"
 
   s3_bucket = data.aws_s3_bucket.lambda_bucket.id
   s3_key = "${var.project_name}/lambda-pkg-${var.build_tag}.zip"
