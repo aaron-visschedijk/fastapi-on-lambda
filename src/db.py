fd8159a7149e10c1697318087f5878a5fceaac53
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-22 07:40:27 +0100

    fix: add region to boto3

--- src/db.py
+++ src/db.py
@@ -1,6 +1,6 @@
 import boto3
 
-db_client = boto3.resource('dynamodb')
+db_client = boto3.resource(service_name='dynamodb', region='eu-central-1')
 table = db_client.Table("users")
 
 def query(user_id):
