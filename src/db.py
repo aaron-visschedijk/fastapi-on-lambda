b5826b11ccaf2ad7bdd6f9553a8c2dd5dc8a25f4
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-22 07:51:04 +0100

    fix: region argument name

--- src/db.py
+++ src/db.py
@@ -1,12 +1,12 @@
 import boto3
 
-db_client = boto3.resource(service_name='dynamodb', region='eu-central-1')
+db_client = boto3.resource(service_name='dynamodb', region_name='eu-central-1')
 table = db_client.Table("users")
 
 def query(user_id):
     response = table.get_item(
         Key={
-            'user_id': "1"
+            'user_id': 1
         }
     )
     return response
