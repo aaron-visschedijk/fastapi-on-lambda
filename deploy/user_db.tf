c6ada4e56e978a56537b059750d3d14da628593b
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-23 17:00:50 +0100

    fix: remove creation date as range key

--- deploy/user_db.tf
+++ deploy/user_db.tf
@@ -1,17 +1,11 @@
 resource "aws_dynamodb_table" "users" {
   name = "users"
   hash_key = "id"
-  range_key = "creation_date"
   read_capacity = 1
   write_capacity = 1
   
   attribute {
     name = "id"
-    type = "S"
-  }
-
-  attribute {
-    name = "creation_date"
     type = "S"
   }
 
@@ -23,7 +17,6 @@ resource "aws_dynamodb_table" "users" {
   global_secondary_index {
     name = "email-index"
     hash_key = "email"
-    range_key = "creation_date"
     projection_type = "KEYS_ONLY"
     write_capacity = 1
     read_capacity = 1
