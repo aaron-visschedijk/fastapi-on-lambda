1e024f91ccbf31fed240a013ee6e9d3b9f8d6d9e
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-02-23 16:30:45 +0100

    feat: add email gsi to user table

--- deploy/user_db.tf
+++ deploy/user_db.tf
@@ -1,12 +1,29 @@
 resource "aws_dynamodb_table" "users" {
   name = "users"
   hash_key = "id"
+  range_key = "creation_date"
   
   attribute {
     name = "id"
     type = "S"
   }
 
+  attribute {
+    name = "creation_date"
+    type = "S"
+  }
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "email-index"
+    hash_key = "email"
+    range_key = "creation_date"
+    projection_type = "KEYS_ONLY"
+  }
 
   read_capacity = 1
   write_capacity = 1
