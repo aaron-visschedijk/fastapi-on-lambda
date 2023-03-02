cd94426e81c984ce224799dfd6b8bcd9a6fdcffb
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-02 12:53:27 +0100

    fix: add cors

--- app/main.py
+++ app/main.py
@@ -6,6 +6,20 @@ import v1.api as api_v1
 app = FastAPI()
 
 
+origins = [
+    "http://localhost:3000",
+    "https://ypp3d3jsvjj3unimeewvydwqyy0iludg.lambda-url.eu-central-1.on.aws/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def root():
     return {"message": "API is live!"}
